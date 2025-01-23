74003d2810291162e288faecfafee17d8e64c506
coara-chocomaru <189215038+coara-chocomaru@users.noreply.github.com>
2025-01-23 23:26:52 +0900

    テスト2.
    .

--- app/src/main/java/com/coara/execapp/MainActivity.java
+++ app/src/main/java/com/coara/execapp/MainActivity.java
@@ -84,16 +84,18 @@ public class MainActivity extends Activity {
 
         // キーボード開閉ボタン
         keyboardButton.setOnClickListener(view -> {
-        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-        if (imm != null) {
-        if (commandInput.hasFocus()) {
-            imm.hideSoftInputFromWindow(commandInput.getWindowToken(), 0);
-        } else {
-            imm.showSoftInput(commandInput, InputMethodManager.SHOW_FORCED);
-        }
-        commandInput.requestFocus();
+            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            if (imm != null) {
+                if (commandInput.hasFocus()) {
+                    imm.hideSoftInputFromWindow(commandInput.getWindowToken(), 0);
+                } else {
+                    imm.showSoftInput(commandInput, InputMethodManager.SHOW_FORCED);
+                }
+                commandInput.requestFocus();
+            }
+        });
     }
-});
+        
     private void checkPermissions() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
             ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
